a8e8da9023ef4c6f7028b95c6388ecc54cd164ae
Viktor Petrov <115278686+viktorptrv@users.noreply.github.com>
2026-04-08 15:08:26 +0300

    Add files via upload

---
+++ Project 2: Text game/android/app/src/main/kotlin/co/appbrewery/destini_challenge_starting/MainActivity.kt
@@ -0,0 +1,5 @@
+package co.appbrewery.destini_challenge_starting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Project 2: Text game/lib/story.dart
@@ -0,0 +1,13 @@
+
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({
+    required this.choice1,
+    required this.choice2,
+    required this.storyTitle
+});
+
+}
